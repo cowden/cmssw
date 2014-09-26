9a8a79b698f65250d6efbf31190c0594374cd33b
Christopher <christopher.cowden@cern.ch>
2014-09-27 01:39:56 +0200

    created ZDC DQM skeleton

--- DQM/HcalMonitorModule/interface/ZDCMonitorModule.h
+++ DQM/HcalMonitorModule/interface/ZDCMonitorModule.h
@@ -1,172 +1,72 @@
-#ifndef ZDCMonitorModule_GUARD_H
-#define ZDCMonitorModule_GUARD_H
+#ifndef ZDCMonitorModule_H
+#define ZDCMonitorModule_H
 
-/*
- * \file ZDCMonitorModule.h
- *
-
- * \author 
- *
-*/
-
-#include "FWCore/Framework/interface/Frameworkfwd.h"
-#include "DQMServices/Core/interface/DQMEDAnalyzer.h"
-#include "FWCore/Framework/interface/Event.h"
-#include "FWCore/Framework/interface/MakerMacros.h"
-#include "FWCore/Framework/interface/ESHandle.h"
-#include "FWCore/Framework/interface/Run.h"
-#include "FWCore/ServiceRegistry/interface/Service.h"
+//Framework
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/Utilities/interface/InputTag.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
 
-#include "CalibFormats/HcalObjects/interface/HcalDbService.h"
-#include "CalibFormats/HcalObjects/interface/HcalDbRecord.h"
+//event
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/EventSetup.h"
 
-#include "DataFormats/HcalDigi/interface/HcalUnpackerReport.h"
-#include "DQM/HcalMonitorTasks/interface/HcalZDCMonitor.h"
+//DQM
+#include "DQMServices/Core/interface/DQMEDAnalyzer.h"
+#include "DQMServices/Core/interface/DQMStore.h"
+#include "DQMServices/Core/interface/MonitorElement.h"
 
-#include "FWCore/Utilities/interface/CPUTimer.h"
+//Candidate handling
+#include "DataFormats/Candidate/interface/Candidate.h"
+#include "DataFormats/Candidate/interface/CandidateFwd.h"
 
-class MonitorElement;
-class  HcalZDCMonitor;
+// Electron
+#include "DataFormats/EgammaCandidates/interface/Electron.h"
+#include "DataFormats/EgammaCandidates/interface/GsfElectron.h"
+#include "DataFormats/EgammaCandidates/interface/GsfElectronFwd.h"
 
-#include <iostream>
-#include <fstream>
+// PFMET
+#include "DataFormats/METReco/interface/PFMET.h"
+#include "DataFormats/METReco/interface/PFMETCollection.h"
 
-class ZDCMonitorModule : public thread_unsafe::DQMEDAnalyzer {
+// Vertex utilities
+#include "DataFormats/VertexReco/interface/Vertex.h"
+#include "DataFormats/VertexReco/interface/VertexFwd.h"
+
+// CaloJets
+#include "DataFormats/JetReco/interface/CaloJet.h"
+
+// Conversions
+#include "RecoEgamma/EgammaTools/interface/ConversionTools.h"
+
+// Trigger
+#include "DataFormats/Common/interface/TriggerResults.h"
+#include "DataFormats/HLTReco/interface/TriggerObject.h"
+#include "DataFormats/HLTReco/interface/TriggerEvent.h"
+#include "FWCore/Common/interface/TriggerNames.h"
+
+ 
+class ZDCMonitorModule: public DQMEDAnalyzer{
 
 public:
-  
-  // Constructor
+
   ZDCMonitorModule(const edm::ParameterSet& ps);
-
-  // Destructor
-  ~ZDCMonitorModule();
-
-  void bookHistograms(DQMStore::IBooker &, edm::Run const &, edm::EventSetup const &);
+  virtual ~ZDCMonitorModule();
   
- protected:
-  
-  // Analyze
-  void analyze(const edm::Event& e, const edm::EventSetup& c);
-  
-  // Begin LumiBlock
-  void beginLuminosityBlock(const edm::LuminosityBlock& lumiSeg, 
-                            const edm::EventSetup& c) ;
+protected:
 
-  // End LumiBlock
-  void endLuminosityBlock(const edm::LuminosityBlock& lumiSeg, 
-                          const edm::EventSetup& c);
+  void dqmBeginRun(edm::Run const &, edm::EventSetup const &) override;
+  void bookHistograms(DQMStore::IBooker &, edm::Run const &, edm::EventSetup const &) override;
+  void analyze(edm::Event const& e, edm::EventSetup const& eSetup);
+  void beginLuminosityBlock(edm::LuminosityBlock const& lumi, edm::EventSetup const& eSetup) ;
+  void endLuminosityBlock(edm::LuminosityBlock const& lumi, edm::EventSetup const& eSetup);
+  void endRun(edm::Run const& run, edm::EventSetup const& eSetup);
 
-  // EndRun
-  void endRun(const edm::Run& run, const edm::EventSetup& c);
+private:
 
-  // Reset
-  void reset(void);
+  MonitorElement *h_evNum;
+  MonitorElement *h_evDen;
 
-  /// Boolean prescale test for this event
-  bool prescale();
-/*
-  // Check ZDC has FED data
-  void CheckZDCStatus	(const FEDRawDataCollection& rawraw,
-                      	const HcalUnpackerReport& report,
-                      	const HcalElectronicsMap& emap,
-                      	const ZDCDigiCollection& zdcdigi
-                      	);
-*/
- private:
-  std::vector<int> fedss;
-  /********************************************************/
-  //  The following member variables can be specified in  //
-  //  the configuration input file for the process.       //
-  /********************************************************/
-
-  /// Prescale variables for restricting the frequency of analyzer
-  /// behavior.  The base class does not implement prescales.
-  /// Set to -1 to be ignored.
-  int prescaleEvt_;    ///units of events
-  int prescaleLS_;     ///units of lumi sections
-  int prescaleTime_;   ///units of minutes
-  int prescaleUpdate_; ///units of "updates", TBD
-
-  // Reset histograms every N events
-  //
-
-  /// keep a reference to the paramter set.  This is needed to pass the 
-  // HcalZDCMonitor helper class
-  const edm::ParameterSet & ps_;
-
-  /// The name of the monitoring process which derives from this
-  /// class, used to standardize filename and file structure
-  std::string monitorName_;
-
-  /// Verbosity switch used for debugging or informational output
-  int debug_;  // make debug an int in order to allow different levels of messaging
-
-  // control whether or not to display time used by each module
-  bool showTiming_; 
-  edm::CPUTimer cpu_timer; // 
-
-  // counters and flags
-  int nevt_;
-
-  struct{
-    timeval startTV,updateTV;
-    double elapsedTime; 
-    double vetoTime; 
-    double updateTime;
-  } psTime_;    
-
-  // environment variables
-  int irun_,ievent_,itime_;
-  unsigned int ilumisec;
-  bool Online_;
-  std::string rootFolder_;
-
-  int ievt_;
-  int ievt_rawdata_;
-  int ievt_digi_;
-  int ievt_rechit_;
-  int ievt_pre_; // copy of counter used for prescale purposes
-  bool fedsListed_;
-  
-  edm::InputTag inputLabelDigi_;
-  edm::InputTag inputLabelRecHitZDC_;
-
-  edm::InputTag FEDRawDataCollection_; // not yet in use, but we still store the tag name
-
-  edm::EDGetTokenT<HcalUnpackerReport> tok_hcal_;
-  edm::EDGetTokenT<ZDCDigiCollection> tok_zdc_;
-  edm::EDGetTokenT<ZDCRecHitCollection> tok_zdcrh_;
-
-  MonitorElement* meIEVTALL_;
-  MonitorElement* meIEVTRAW_;
-  MonitorElement* meIEVTDIGI_;
-  MonitorElement* meIEVTRECHIT_;
-
-  MonitorElement* meFEDS_;
-  MonitorElement* meStatus_;
-  MonitorElement* meTrigger_;
-  MonitorElement* meLatency_;
-  MonitorElement* meQuality_;
-  
-  HcalZDCMonitor*         zdcMon_;
-
-  ////---- decide whether the ZDC status should be checked
-  bool checkZDC_;
-
-  edm::ESHandle<HcalDbService> conditions_;
-  const HcalElectronicsMap*    readoutMap_;
-
-  std::ofstream m_logFile;
-
-  // Determine whether the ZDC in the run (using FED info)
-  int ZDCpresent_;
-  MonitorElement* meZDC_;
-
-  // myquality_ will store status values for each det ID I find
-  bool dump2database_;
-  //std::map<HcalDetId, unsigned int> myquality_;
-  //HcalChannelQuality* chanquality_;
 };
+
 
 #endif
